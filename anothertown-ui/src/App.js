b8efa1cf27c544baa8208a5b749c4818da39a290
Kristen <69090874+buonicannoli@users.noreply.github.com>
2020-08-01 14:11:51 -0700

    setup app

---
+++ anothertown-ui/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import MapChart from './components/map-chart'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <div>Another Town</div>
+       
+      </header>
+
+      <MapChart/>
+    </div>
+  );
+}
+
+export default App;
