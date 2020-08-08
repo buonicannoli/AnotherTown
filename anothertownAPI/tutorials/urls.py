111a081037df51eb367e3f10304f9ee99eed11ab
Kristen <69090874+buonicannoli@users.noreply.github.com>
2020-08-07 21:22:55 -0700

    data additions

--- anothertownAPI/tutorials/urls.py
+++ anothertownAPI/tutorials/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
+from django.conf.urls import url
+
+from .views import TutorialRudView
 
 urlpatterns = [
     path('api/tutorial/', views.TutorialListCreate.as_view() ),
+    url(r'^(?P<pk>\d+)/$', TutorialRudView.as_view())
 ]
--- anothertownAPI/tutorials/views.py
+++ anothertownAPI/tutorials/views.py
@@ -1,7 +1,22 @@
 from .models import Tutorial
 from .serializers import TutorialSerializer
 from rest_framework import generics
+from django.db.models import Q
 
 class TutorialListCreate(generics.ListCreateAPIView):
     queryset = Tutorial.objects.all()
     serializer_class = TutorialSerializer
+
+class TutorialRudView(generics.RetrieveUpdateDestroyAPIView): # DetailView CreateView FormView
+    # lookup_fields            =  ['title', 'description'] #pk' #, id # url(r'?P<pk>\d+')
+    serializer_class        = TutorialSerializer
+    # permission_classes      = [IsOwnerOrReadOnly]
+    queryset                = Tutorial.objects.all()
+
+
+    def get_queryset(self):
+        print(Tutorial.objects.all())
+        return 20
+
+    def get_serializer_context(self, *args, **kwargs):
+        return {"request": self.request}
